3695a044064bd836e9f721f7afc1ecbef49a07b9
Domenique412 <dwatkinswise@gmail.com>
2025-11-03 17:20:31 -0500

    files stuctered

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
-
 const { PORT = 3001 } = process.env;
-const yes = {};
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db')
+  .then(() => {
+    console.log("CONNECTED TO  DB PLAYA!!!!");
+  })
+  .catch(console.error);
+
 
 app.listen(PORT, () => {
   console.log(`listeing on port ${PORT}`)
